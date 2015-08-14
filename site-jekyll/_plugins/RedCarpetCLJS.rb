739a6161f272dc23339b856f8f0e4f050e10529a
Shaun Williams <shaun.e.williams@gmail.com>
2015-08-14 11:12:04 -0500

    create markdown preprocessor for cljs-specific things

---
+++ site-jekyll/_plugins/RedCarpetCLJS.rb
@@ -0,0 +1,34 @@
+# We just want to make some ClojureScript-specific things to the markdown processor:
+# - CLJS-NNNN -> jira issue links
+# - ...
+
+require 'jekyll'
+require 'redcarpet'
+
+def preprocess_for_cljs(content)
+  content
+end
+
+# THIS IS PASTED from the jekyll repo v2.5.3:
+# <https://github.com/jekyll/jekyll/blob/v2.5.3/lib/jekyll/converters/markdown/redcarpet_parser.rb#L94-L99>
+# 
+# If we need to update Jekyll, check the version in Gemfile.lock,
+# then repaste the "convert" method here, then modify.
+#
+# The only modification is the creation and usage of 'extended_render' in place of '@renderer'
+class Jekyll::Converters::Markdown::RedcarpetCLJS < Jekyll::Converters::Markdown::RedcarpetParser
+  def convert(content)
+    @redcarpet_extensions[:fenced_code_blocks] = !@redcarpet_extensions[:no_fenced_code_blocks]
+    @renderer.send :include, Redcarpet::Render::SmartyPants if @redcarpet_extensions[:smart]
+
+    extended_renderer = Class.new(@renderer) do
+      def preprocess(content)
+        preprocess_for_cljs(content)
+      end
+    end
+
+    markdown = Redcarpet::Markdown.new(extended_renderer.new(@redcarpet_extensions), @redcarpet_extensions)
+    markdown.render(content)
+  end
+end
+

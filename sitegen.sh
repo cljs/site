4130d24a92fb2867656943876357dae611b28dbf
Shaun Williams <shaun.e.williams@gmail.com>
2016-04-09 16:12:04 -0500

    allow planck to use deps retrieved from boot

--- sitegen.sh
+++ sitegen.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
+# Planck allows us to run tasks written in ClojureScript,
+# which we use to generate static site pages.
+# We can eventually replace this
+
+# Cache the classpath so boot doesn't slow down our build.
+# This allows Planck to use dependencies.
+if [ ! -f .classpath ]; then
+  classpath=`boot show -c | tee .classpath`
+else
+  classpath=`cat .classpath`
+fi
+
 planck \
-  -c src \
+  -c src:$classpath \
   -m sitegen.core

0e3905c4eb93380cb4bdf6712d442737cf5472ea
Shaun Williams <shaun.e.williams@gmail.com>
2016-04-13 11:53:15 -0500

    create api doc urls

--- src/sitegen/urls.cljs
+++ src/sitegen/urls.cljs
@@ -6,11 +6,14 @@
 
 (def root "http://cljsinfo.github.io")
 
-(def  home                    "/index.html")
-(def  news-dir                "/news/")
-(def  news-index              "/news/index.html")
-(def  news-feed               "/news/feed.xml")
-(defn news-post  [title] (str "/news/" title ".html"))
+(def  home                            "/index.html")
+(def  news-dir                        "/news/")
+(def  news-index                      "/news/index.html")
+(def  news-feed                       "/news/feed.xml")
+(defn news-post  [title]         (str "/news/" title ".html"))
+(def  docs-dir                        "/docs/")
+(defn api-ns     [ns]            (str "/docs/" ns))
+(defn api-symbol [ns sym-encode] (str "/docs/" ns "/" sym-encode ".html"))
 
 (def out-dir "output")
 (defn write! [url content] (spit (str out-dir url) content))

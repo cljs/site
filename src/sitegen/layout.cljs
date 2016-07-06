8d6f0a62e299b95b90533144cddf741b578d08ba
Shaun Williams <shaun.e.williams@gmail.com>
2016-07-06 00:12:10 -0500

    switch to Open Sans typeface

--- src/sitegen/layout.cljs
+++ src/sitegen/layout.cljs
@@ -37,7 +37,7 @@
     [:title title]
     [:meta {:name "description" :content description}]
 
-    [:link {:rel "stylesheet" :href "http://fonts.googleapis.com/css?family=Raleway:400,300,600,700"}]
+    [:link {:rel "stylesheet" :href "http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700"}]
     [:link {:rel "stylesheet" :href "https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css"}]
     [:link {:rel "stylesheet" :href "/css/normalize.css"}]
     [:link {:rel "stylesheet" :href "/css/skeleton.css"}]

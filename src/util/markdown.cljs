48e0bc8fcbc2a0979052bf89d14948b0a390c18e
Shaun Williams <shaun.e.williams@gmail.com>
2016-08-07 17:48:13 -0700

    keep code links from being underlined

--- src/util/markdown.cljs
+++ src/util/markdown.cljs
@@ -1,11 +1,21 @@
 (ns util.markdown
   (:require
-    [util.highlight :refer [highlight-code]]))
+    [util.highlight :refer [highlight-code]]
+    [util.hiccup :as hiccup]
+    [clojure.string :as string]))
 
 (def marked (js/require "marked"))
 
+(def renderer (marked.Renderer.))
+(set! renderer.link
+  (fn [href title text]
+    (let [className (when (string/starts-with? text "<code") "code-link")]
+      (hiccup/render
+        [:a {:href href :title title :class className} text]))))
+
 (.setOptions marked
-  #js {:highlight highlight-code})
+  #js {:highlight highlight-code
+       :renderer renderer})
 
 (defn render [md]
   (marked md))

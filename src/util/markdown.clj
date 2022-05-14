176770758156850141728dd546450a107227df5d
Shaun Williams <shaunewilliams@gmail.com>
2022-05-14 13:44:52 -0500

    use commonmark-java

--- src/util/markdown.clj
+++ src/util/markdown.clj
@@ -1,15 +1,22 @@
 (ns util.markdown
   (:require
     [clojure.string :as str]
-    [markdown.core :refer [md-to-html-string]]))
+    [markdown.core :refer [md-to-html-string]])
+  (:import
+    [org.commonmark.node Node]
+    [org.commonmark.parser Parser]
+    [org.commonmark.renderer.html HtmlRenderer]))
+
+(def ^Parser parser         (.build (Parser/builder)))
+(def ^HtmlRenderer renderer (.build (HtmlRenderer/builder)))
 
 (defn render [md]
-  (-> md
-      (str/replace #"(?m)^\[[^\]]+\]:" #(str %1 " ")) ;; need to add a space in front of reference links "[foo]:bar" -> "[foo]: bar"
-      (md-to-html-string :reference-links? true)))
+  (let [^Node document (.parse parser md)]
+    (.render renderer document)))
 
 (comment
-  (def s " This is [foo][foo].
-[foo]:../../api/syntax/syntax destructure-vector.html")
+  (def s " This is [foo].
+
+[foo]:../../api/syntax/syntax_destructure-vector.html")
 
   (render s))
